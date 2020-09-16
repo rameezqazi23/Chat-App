54e964cbbec8f96a15a3543af28a3c2530c2bd39
Rameez Qazi <38956101+rameezqazi23@users.noreply.github.com>
2020-09-17 00:52:40 +0500

    ctreating end-user nodes

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 function chatApp(){
-    message = document.getElementById("button-addon2-target")
+    var message = document.getElementById("msg-text")
     console.log(message)
+    
+    var p = document.createElement('p')
+    console.log(p)
+
+    var val1 = document.createTextNode(message.value)
+    p.appendChild(val1)
+
+    var chat = document.getElementById('chat')
+    chat.appendChild(p)
+
+    message.value = ""
 }
